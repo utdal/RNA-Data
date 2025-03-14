ad3268b90b24876a696f2a478b4564743e345416
Hemanth Mydugolam <132294355+HemanthMydugolam@users.noreply.github.com>
2025-03-14 13:22:52 -0500

    Update Google_sheets.R

--- Google_sheets.R
+++ Google_sheets.R
@@ -2,5 +2,5 @@ library(googlesheets4)
 
 gs4_auth()
 
-ss <- gs4_get("https://docs.google.com/spreadsheets/d/1Ma-ZYlGw-U0haJw2fStVYxyT99buWj4WMCKePfqvaww/edit#gid=0")
+ss <- gs4_get("link")
 sheet_append(ss, data.frame(time=Sys.time()))
